fd479bc6a2d38bab88e5482798b2aa84dc29aa4b
Michael lemito <delovoy.lemit@gmail.com>
2025-02-23 13:00:22 +0300

    выглядит как что-то похожее

---
+++ include/base.h
@@ -0,0 +1,16 @@
+//
+// Created by lemito on 2/23/25.
+//
+
+#ifndef BASE_H
+#define BASE_H
+
+enum STATUS_CODE {
+	SUCCESS = 0,
+	INPUT_ERROR = -1,
+	NULL_PTR = -2,
+	ERROR_OPEN = -3
+};
+typedef enum STATUS_CODE STATUS_CODE;
+
+#endif //BASE_H
