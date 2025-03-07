9ffecd217a31538da1834e21835ebf2e4b3e50f0
Michael lemito <delovoy.lemit@gmail.com>
2025-03-07 12:01:06 +0300

    4

--- include/base.h
+++ include/base.h
@@ -6,29 +6,39 @@
 #define BASE_H
 
 enum STATUS_CODE {
-    SUCCESS = 0,
-    INPUT_ERROR,
-    NULL_PTR,
-    ERROR_OPEN,
-    MEMORY_ERROR,
-    USER_NOT_CONTAIN,
-    USER_IS_CONTAIN,
-    SEM_ERR,
-    FORK_ERROR,
-    THREAD_ERROR
+  SUCCESS = 0,
+  INPUT_ERROR,
+  NULL_PTR,
+  ERROR_OPEN,
+  MEMORY_ERROR,
+  USER_NOT_CONTAIN,
+  USER_IS_CONTAIN,
+  SEM_ERR,
+  FORK_ERROR,
+  THREAD_ERROR
 };
 
 typedef enum STATUS_CODE STATUS_CODE;
 
 union semun {
-    int val; /* значение для SETVAL */
-    struct semid_ds *buf; /* буфер для IPC_STAT, IPC_SET */
-    unsigned short *array; /* массив для GETALL, SETALL */
-    struct seminfo *__buf; /* буфер для IPC_INFO (Linux-specific) */
+  int val;               /* значение для SETVAL */
+  struct semid_ds *buf;  /* буфер для IPC_STAT, IPC_SET */
+  unsigned short *array; /* массив для GETALL, SETALL */
+  struct seminfo *__buf; /* буфер для IPC_INFO (Linux-specific) */
 };
 
-#define CLEAR_BUF() while (getchar() != '\n'){}
+#define CLEAR_BUF()           \
+  while (getchar() != '\n') { \
+  }
 
-#define FREE_AND_NULL(ptr) if (ptr != NULL){ free(ptr); ptr = NULL; }
+#define FREE_AND_NULL(ptr) \
+  if (ptr != NULL) {       \
+    free(ptr);             \
+    ptr = NULL;            \
+  }
+#define FCLOSE(fp)  \
+  if (fp != NULL) { \
+    fclose(fp);     \
+  }
 
-#endif //BASE_H
+#endif  // BASE_H
