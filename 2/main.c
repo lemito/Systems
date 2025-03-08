c85ab30e7ef37a6fa6ad1f00330fee079814555c
Michael lemito <delovoy.lemit@gmail.com>
2025-03-08 17:37:39 +0300

    огромный рефакторинг

--- 2/main.c
+++ 2/main.c
@@ -6,13 +6,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/msg.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 #include "../include/base.h"
 
-#define pow2(n) (1 << (n))
+#define MSG_SIZE 6144
+
+typedef struct msg_text {
+  int qid;
+  char buf[MSG_SIZE];
+} msg_text;
+
+typedef struct msg {
+  long mtype;
+  msg_text data;
+} msg;
+
 typedef char byte;
+
+char check_parallel(const char *action) {
+  if (action == 0) {
+    return 0;
+  }
+
+  if (strncmp(action, "copy", strlen("copy")) == 0 ||
+      strncmp(action, "find", strlen("find")) == 0) {
+    return 1;
+  }
+
+  return 0;
+}
 
 STATUS_CODE check_hex(const char *hex) {
   if (hex == NULL) {
@@ -55,6 +80,7 @@ STATUS_CODE xor2(FILE *file) {
   while (fread(&one, sizeof(byte), 1, file)) {
     // байт == 8 бит; сдвигаемся на 4 чтобы получить верхнюю 4 битов
     const byte high_byte = one >> 4;
+    // const byte high_bytes = one & 0xF0;
     // получаем последние 4 бита
     const byte low_byte = one & 0x0F;
     res ^= high_byte;
@@ -178,6 +204,7 @@ STATUS_CODE xor6(FILE *file) {
   return SUCCESS;
 }
 
+// SUCESS - найдено; ERROR_OPEN - не найдено
 STATUS_CODE find(FILE *file, const char **const attr) {
   if (file == NULL || attr == NULL || *attr == NULL) {
     return INPUT_ERROR;
@@ -327,6 +354,7 @@ STATUS_CODE cnt_by_mask(const int *mask, size_t *res, FILE *file) {
   return SUCCESS;
 }
 
+/*
 STATUS_CODE do_action(FILE **file, const char **const action,
                       const char **const attr, const char **const filename) {
   if (action == NULL || *action == NULL || attr == NULL || *attr == NULL ||
@@ -418,14 +446,17 @@ STATUS_CODE do_action(FILE **file, const char **const action,
       }
       case -1:
         // return FORK_ERROR;
-          exit(FORK_ERROR);
+        exit(FORK_ERROR);
       default: {
         wait(&status);  // ждемс чилда, пока он там все поищет
         // printf("st=%d\n",status);
         switch (status) {
-          case MEMORY_ERROR: {return MEMORY_ERROR;}
-          // case SUCCESS: {printf("%s был найден в %s\n", *attr, BUF);} break;
-          // case NULL_PTR: {printf("%s не нашлось в %s :(", *attr, BUF);}
+          case MEMORY_ERROR: {
+            return MEMORY_ERROR;
+          }
+            // case SUCCESS: {printf("%s был найден в %s\n", *attr, BUF);}
+            // break; case NULL_PTR: {printf("%s не нашлось в %s :(", *attr,
+            // BUF);}
         }
       }
     }
@@ -435,18 +466,224 @@ STATUS_CODE do_action(FILE **file, const char **const action,
 
   return SUCCESS;
 }
+*/
 
-//
-// Created by lemito on 2/23/25.
-//
-int main(const int argc, char *argv[]) {
-  if (argc < 3) {
-    printf("Используй: %s <file> <file> <флаг>\n", argv[0]);
+STATUS_CODE xor_action(char **files, int size, const int N) {
+  if (files == NULL || N < 2 || N > 7) {
+    return INPUT_ERROR;
+  }
+  size -= 1;
+  for (size_t i = 0; i < size; i++) {
+    FILE *fp = NULL;
+    fp = fopen(files[i], "rb");
+    if (fp == NULL) {
+      printf("ошибка файла %s\n", files[i]);
+
+      return ERROR_OPEN;
+    }
+    printf("Результат для файла %s: \n", files[i]);
+    switch (N) {
+      case 2: {
+        xor2(fp);
+      } break;
+      case 3: {
+        xor3(fp);
+      } break;
+      case 4: {
+        xor4(fp);
+      } break;
+      case 5: {
+        xor5(fp);
+      } break;
+      case 6: {
+        xor6(fp);
+      } break;
+      default: {
+        printf("Упс... N = [2...6]\n");
+      } break;
+    }
+    FCLOSE(fp);
+    printf("====\n");
+  }
+  return SUCCESS;
+}
+
+STATUS_CODE copy_action(char **files, int size, const int N) {
+  if (files == NULL || N < 2 || N > 7) {
+    return INPUT_ERROR;
+  }
+  size -= 1;
+  for (size_t i = 0; i < size; i++) {
+    FILE *fp = NULL;
+    fp = fopen(files[i], "rb");
+    if (fp == NULL) {
+      printf("ошибка файла %s\n", files[i]);
+
+      return ERROR_OPEN;
+    }
+
+    const pid_t pid = fork();
+    switch (pid) {
+      case -1: {
+        perror("fork");
+        continue;
+      }
+      case 0: {
+        if (SUCCESS != copy(&fp, N, files + i)) {
+          printf("mew");
+          exit(ERROR_OPEN);
+        }
+        exit(SUCCESS);
+      }
+      default: {
+      } break;
+    }
+
+    FCLOSE(fp);
+  }
+  return SUCCESS;
+}
+
+STATUS_CODE mask_action(char **files, int size, const char *attr) {
+  if (files == NULL || attr == NULL) {
+    return INPUT_ERROR;
+  }
+  size -= 1;
+  for (size_t i = 0; i < size; i++) {
+    FILE *fp = NULL;
+    fp = fopen(files[i], "rb");
+    if (fp == NULL) {
+      printf("ошибка файла %s\n", files[i]);
+
+      return ERROR_OPEN;
+    }
+    int mask = 0;
+    if (ERROR_OPEN == check_hex(attr)) {
+      return INPUT_ERROR;
+    }
+    if (SUCCESS != str_to_uint(attr, &mask, 16)) {
+      return INPUT_ERROR;
+    }
+    size_t res = 0;
+    if (INPUT_ERROR == cnt_by_mask(&mask, &res, fp)) {
+      return INPUT_ERROR;
+    }
+    printf(
+        "Четырехбайтовых чисел, удовлетворяющих маске %s найдено %ld в файле "
+        "%s\n",
+        attr, res, files[i]);
+    FCLOSE(fp);
+  }
+  return SUCCESS;
+}
+
+typedef struct data {
+  pid_t pid;
+  const char *filename;
+} data_t;
+
+STATUS_CODE find_action(char **files, int size, const char *attr) {
+  if (files == NULL || attr == NULL) {
     return INPUT_ERROR;
   }
 
-  const char *action = NULL;
-  const char *attr = NULL;
+  const int msgqid = msgget(IPC_PRIVATE, IPC_CREAT | 0666);
+  if (msgqid == -1) {
+    perror("msgget");
+    return INPUT_ERROR;
+  }
+  size -= 1;
+  data_t *child_info = malloc(size * sizeof(data_t));
+  if (NULL == child_info) {
+    return MEMORY_ERROR;
+  }
+  size_t child_cnt = 0;
+
+  for (size_t i = 0; i < size; i++) {
+    FILE *fp = NULL;
+    fp = fopen(files[i], "rb");
+    if (fp == NULL) {
+      FREE_AND_NULL(child_info);
+      const int st = msgctl(msgqid, IPC_RMID, NULL);
+      if (st == -1) {
+        return SEM_ERR;
+      }
+      printf("ошибка файла %s\n", files[i]);
+      return ERROR_OPEN;
+    }
+
+    const pid_t pid = fork();
+    switch (pid) {
+      case -1: {
+        perror("fork");
+        continue;
+      }
+      case 0: {
+        char BUF[FILENAME_MAX];
+        msg res = {0};
+
+        if (NULL == get_absolute_path(files[i], BUF)) {
+          exit(MEMORY_ERROR);
+        }
+        const int st = find(fp, &attr);
+        res.mtype = getpid();
+        res.data.qid = getpid();
+        // printf("%d\n", msgqid);
+        if (st == SUCCESS) {
+          snprintf(res.data.buf, MSG_SIZE, "%s был найден в %s\0", attr, BUF);
+        } else if (st == ERROR_OPEN) {
+          snprintf(res.data.buf, MSG_SIZE, "%s не был найден в %s\0", attr,
+                   BUF);
+        }
+        if (msgsnd(msgqid, &res, sizeof(res.data), 0) == -1) {
+          perror("msgsnd");
+          exit(INPUT_ERROR);
+        }
+        exit(SUCCESS);
+      }
+      default: {
+        child_info[child_cnt].pid = pid;
+        child_info[child_cnt].filename = files[i];
+        child_cnt++;
+      } break;
+    }
+
+    FCLOSE(fp);
+  }
+
+  if (child_cnt == 0) {
+    fprintf(stderr, "Не удалось обработать ни одного файла\n");
+    FREE_AND_NULL(child_info);
+    msgctl(msgqid, IPC_RMID, NULL);
+    return ERROR_OPEN;
+  }
+
+  for (int i = 0; i < child_cnt; i++) {
+    waitpid(child_info[i].pid, NULL, 0);
+    msg res;
+    if (msgrcv(msgqid, &res, sizeof(res.data), child_info[i].pid, 0) == -1) {
+      perror("msgrcv");
+      continue;
+    }
+    printf("%s\n", res.data.buf);
+  }
+
+  FREE_AND_NULL(child_info);
+
+  const int st = msgctl(msgqid, IPC_RMID, NULL);
+  if (st == -1) {
+    return SEM_ERR;
+  }
+  return SUCCESS;
+}
+
+STATUS_CODE work_with_files(char **argv, const int argc) {
+  if (argv == NULL) {
+    return NULL_PTR;
+  }
+
+  const char *action = NULL;  // действие
+  const char *attr = NULL;    // параметр
   int size = 0;
 
   action = (strcmp(argv[argc - 2], "mask") == 0 ||
@@ -459,22 +696,63 @@ int main(const int argc, char *argv[]) {
              ? argc - 2
              : argc - 1;
 
-  for (int i = 1; i < size; i++) {
-    if (argv[i] == NULL) {
-      return INPUT_ERROR;
-    }
-    FILE *file = fopen(argv[i], "rb");
-    if (file == NULL) {
-      printf("Ошибка открытия файла");
-      return INPUT_ERROR;
-    }
-    STATUS_CODE st = do_action(&file, &action, &attr, &argv[i]);
-    if (st != SUCCESS) {
-      fclose(file);
-      return st;
-    }
-    fclose(file);
+  if (action == NULL) {
+    return MEMORY_ERROR;
   }
+  if (strncmp(action, "xor", 3) == 0) {
+    const int N = action[3] - '0';
+    return xor_action(argv + 1, size, N);
+  }
+  if (strncmp(action, "copy", 4) == 0) {
+    const size_t len = strlen(action);
+    int N = 0;  // количество копий файлов
+    size_t pow = 1;
+    for (size_t i = 4; i < len; i++) {
+      N += (action[i] - '0') * pow;
+      pow *= 10;
+    }
+    return copy_action(argv + 1, size, N);
+  }
+  if (strncmp(action, "mask", 4) == 0) {
+    return mask_action(argv + 1, size, attr);
+  }
+  if (strncmp(action, "find", 4) == 0) {
+    return find_action(argv + 1, size, attr);
+  }
+  return INPUT_ERROR;
+}
+//
+// Created by lemito on 2/23/25.
+//
+int main(const int argc, char *argv[]) {
+  if (argc < 3) {
+    printf("Используй: %s <file> <file> <флаг>\n", argv[0]);
+    return INPUT_ERROR;
+  }
+
+  const STATUS_CODE st = work_with_files(argv, argc);
+  if (st != SUCCESS) {
+    return st;
+  }
+
+  // for (int i = 1; i < size; i++) {
+  //   if (argv[i] == NULL) {
+  //     return INPUT_ERROR;
+  //   }
+  //
+  //   FILE *file = fopen(argv[i], "rb");
+  //   if (file == NULL) {
+  //     printf("Ошибка открытия файла");
+  //     return INPUT_ERROR;
+  //   }
+  //   const STATUS_CODE st = do_action(&file, &action, &attr, &argv[i]);
+  //   if (st != SUCCESS) {
+  //     FCLOSE(file);
+  //     return st;
+  //   }
+  //
+  //   FCLOSE(file);
+  // }
 
   return 0;
 }
