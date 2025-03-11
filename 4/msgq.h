dffe1d1f702a14e51be14b91129b8cd323736299
Michael lemito <delovoy.lemit@gmail.com>
2025-03-11 20:52:15 +0300

    доп идея

--- 4/msgq.h
+++ 4/msgq.h
@@ -1,21 +1,27 @@
 #ifndef MSGQ_H
 #define MSGQ_H
 
-#include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <sys/types.h>
 
 #define MSGQ_KEY "/home/lemito/Desktop/my_sems/msgq"
 #define PROJECT_ID 'l'
 
 typedef struct msg_text {
-    int qid;
-    char buf[40];
-  } msg_text;
-  
-  typedef struct msg {
-    long msg_type;
-    msg_text data;
-  } msg;
+  int qid;
+  char buf[40];
+} msg_text;
+
+typedef struct msg {
+  long msg_type;
+  msg_text data;
+} msg;
+
+// для реги
+struct reg_msg {
+  long mtype;
+  pid_t client_pid;
+};
 
 #endif
